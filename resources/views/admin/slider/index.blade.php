6a756d3aa8e2fe5176b76998b62efe254046dbd1
putriardi <putriardi407@gmail.com>
2018-11-26 11:00:54 +0700

    slider

--- resources/views/admin/slider/index.blade.php
+++ resources/views/admin/slider/index.blade.php
@@ -3,7 +3,7 @@
 @section('title','Slider')
 
 @push('css')
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap.min.css">
+    
 @endpush 
 
 @section('content')
@@ -62,11 +62,5 @@
 
 
 @push('scripts')
-    <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap.min.js"></script>
-    <script>
-        $(document).ready(function() {
-            $('#table').DataTable();
-        } );
-    </script>
+   
 @endpush
